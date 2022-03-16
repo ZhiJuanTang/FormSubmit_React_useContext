a530d40915844d740ff03621cc66618315f29acd
cc8carat <chloe26.ni@gmail.com>
2022-03-16 12:01:50 +0100

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import { ImFacebook, ImTwitter, ImLinkedin2 } from 'react-icons/im';
+import reward from '../assets/best-of-internet-service.png';
+
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='container-content'>
+        <div className='container-footer'>
+          <div className='reward'>
+            <img src={reward} />
+          </div>
+          <ul className='footer-item'>
+            <li>About us</li>
+            <li>Become a Partner</li>
+            <li>FAQ</li>
+          </ul>
+          <ul className='footer-item'>
+            <li>Imprint</li>
+            <li>Terms &amp; Conditions</li>
+            <li>Privacy Policy</li>
+          </ul>
+          <ul className='footer-item'>
+            <li>support@snubes.com</li>
+            <li>+49 (0) 305 5645327</li>
+            <li>
+              <span>
+                <ImFacebook />
+              </span>
+              <span>
+                <ImTwitter />
+              </span>
+              <span>
+                <ImLinkedin2 />
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
